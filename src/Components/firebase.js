097ee23ef2de3bef25e1fe2b6692816b3c475644
shivam534d <shivam534d@gmail.com>
2022-09-12 15:16:36 +0530

    Firebase Auth Test

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -4,6 +4,14 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-firebase.initializeApp({});
+firebase.initializeApp({
+  apiKey: 'AIzaSyAbO4ZdXRDYDO2uWAPIvrH-SNBV_O6LBnE',
+  authDomain: 'reels534d.firebaseapp.com',
+  projectId: 'reels534d',
+  storageBucket: 'reels534d.appspot.com',
+  messagingSenderId: '163717546663',
+  appId: '1:163717546663:web:ddd5e2de9b34e4b1b11182',
+  measurementId: 'G-3FK9YQ31LZ',
+});
 
 export default firebase;
