c96f923d896275dd6dae0c4c687a155e50d40112
shivam534d <shivam534d@gmail.com>
2022-09-12 14:15:59 +0530

    Firebase Auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import firebase from './Components/firebase';
 
 function App() {
+
+  const auth = firebase.auth();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [passwprd, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label htmlFor="email">
+        <input type="email" name="email" id="email" />
+      </label>
+      <label htmlFor="pass">
+        <input type="password" name="pass" id="pass" />
+      </label>
+    </>
   );
 }
 
---
+++ src/Components/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAbO4ZdXRDYDO2uWAPIvrH-SNBV_O6LBnE",
+    authDomain: "reels534d.firebaseapp.com",
+    projectId: "reels534d",
+    storageBucket: "reels534d.appspot.com",
+    messagingSenderId: "163717546663",
+    appId: "1:163717546663:web:ddd5e2de9b34e4b1b11182",
+    measurementId: "G-3FK9YQ31LZ"
+});
+
+export default firebase;
